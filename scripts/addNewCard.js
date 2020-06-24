f2d6947b4bda7c116ba33095b2ca48c9816908ec
bogdan <varlamovbogdan@yandex.ru>
2020-06-24 11:06:59 +0400

    fix js file

--- scripts/addNewCard.js
+++ scripts/addNewCard.js
@@ -1,4 +1,3 @@
-
 //массив с карточками
 const initialCards = [
   {
@@ -26,32 +25,77 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// добавление карточки на страницу
+// функция лайк для карточки
+function makeLike() {
+  const likeButtons = document.querySelectorAll('.gallery__button');
+  likeButtons.forEach(likeButton => {
+    likeButton.addEventListener('click', function (event) {
+      if (event.target === likeButton) {
+        likeButton.classList.toggle('gallery__button_selected');
+      }
+    })
+  })
+}
 
+// функция удаление карточки
+function deleteCard() {
+  const trashButtons = document.querySelectorAll('.gallery__trash-button');
+  trashButtons.forEach(trashButton => {
+    trashButton.addEventListener('click', function () {
+      const listItem = trashButton.closest('.gallery__element');
+      listItem.remove();
+    })
+  })
+}
+
+// функция закрытия большой картинки
+const full = document.querySelector('.full');
+const fullCloseBtn = full.querySelector('.full__close');
+function toggleFull() {
+  full.classList.toggle('full_opened');
+}
+fullCloseBtn.addEventListener('click', function () {
+  toggleFull();
+});
+
+
+const fullImage = full.querySelector('.full__image');
+const fullTitle = full.querySelector('.full__title');
+
+//функция открытия базовых шести картинок на весь экран 75 на 75
+function OpenFullImage() {
+  const cardsForFull = document.querySelectorAll('.gallery__element');
+  cardsForFull.forEach(element =>
+    element.querySelector('.gallery__img').addEventListener('click', function () {
+      const elementImg = element.querySelector('.gallery__img');
+      const elementTitle = element.querySelector('.gallery__element-title');
+      fullImage.src = elementImg.src;
+      fullTitle.textContent = elementTitle.textContent;
+      toggleFull();
+    })
+  )
+}
+
+// добавление начальных карточек на страницу
 const card = document.querySelector('#card').content;
 const gallery = document.querySelector('.gallery__elements');
 
-
-// цикл для добавления каждой карчтоки на странцу
-
+// добавление каждой карточки на страницу
 initialCards.forEach((initialCard) => {
   const elementCard = card.cloneNode(true);
   elementCard.querySelector('.gallery__img').src = initialCard.link;
   elementCard.querySelector('.gallery__element-title').textContent = initialCard.name;
-
-  // //добавляем фунцию работы с trash-button
-  // deleteCard();
-
-  // //добавляем функция рыботы с like-button
-  // makeLike();
-
-  //добавляем карточки на страницу
+  //добавляем в DOM дерево
   gallery.append(elementCard);
 })
+//добавляем нужные функции
+makeLike();
+deleteCard();
+OpenFullImage();
 
-//----------
-
+//форма добавления новых карточек
 const creatNewCardBtn = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup-add');
 const exitPopupAdd = document.querySelector('.popup-add__button-exit');
@@ -59,11 +103,9 @@ const exitPopupAdd = document.querySelector('.popup-add__button-exit');
 //форма
 const popupAddForm = document.querySelector('.popup-add__form');
 
-
 // поля ввода название карточки и ссылка на картинку
 const cardTitle = document.querySelector('.popup-add__input_text_name');
 const cardSrc = document.querySelector('.popup-add__input_text_caption');
-
 
 function togglePopupAdd() {
   popupAdd.classList.toggle('popup-add_opened');
@@ -72,14 +114,21 @@ function togglePopupAdd() {
 creatNewCardBtn.addEventListener('click', togglePopupAdd);
 exitPopupAdd.addEventListener('click', togglePopupAdd);
 
-// const nameCard = cardTitle.value;
-// const linkCard = cardSrc.value;
+popupAddForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  makeNewCard();
+  togglePopupAdd();
+  cardTitle.value = '';
+  cardSrc.value = '';
+});
 
+// работа с новой созданной карточкой
 function makeNewCard() {
   const newCard = {
     name: cardTitle.value,
     link: cardSrc.value
   }
+  //добавляем отдельную новую карточку
   initialCards.push(newCard);
   const elementCard = card.cloneNode(true);
   elementCard.querySelector('.gallery__img').src = newCard.link;
@@ -93,49 +142,15 @@ function makeNewCard() {
 
   // //добавляем функция рыботы с like-button
   makeLike();
-}
 
-popupAddForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  makeNewCard();
-  togglePopupAdd();
-});
-
-
-
-
-
-
-
-//-------------------
-
-// функция лайк
-function makeLike() {
-  const likeButtons = document.querySelectorAll('.gallery__button');
-  likeButtons.forEach(likeButton => {
-    likeButton.addEventListener('click', function (event) {
-      if (event.target === likeButton) {
-        likeButton.classList.toggle('gallery__button_selected');
-      }
-    })
+  // открытие новой, созданной отдельно картинки на весь экран
+  const element = document.querySelector('.gallery__element');
+  element.querySelector('.gallery__img').addEventListener('click', function () {
+    const elementImg = element.querySelector('.gallery__img');
+    const elementTitle = element.querySelector('.gallery__element-title');
+    fullImage.src = elementImg.src;
+    fullTitle.textContent = elementTitle.textContent;
+    toggleFull();
   })
 }
-makeLike();
-
-// удаление карточки
-function deleteCard() {
-  const trashButtons = document.querySelectorAll('.gallery__trash-button');
-
-  trashButtons.forEach(trashButton => {
-    trashButton.addEventListener('click', function () {
-      const listItem = trashButton.closest('.gallery__element');
-      listItem.remove();
-    })
-  })
-}
-deleteCard();
-
-
-
-//открытие картини на полный экран 75 на 75
 
---
+++ scripts/openFullPicture.js
@@ -0,0 +1,8 @@
+// const full = document.querySelector('.full');
+// const fullCloseBtn = full.querySelector('.full__close');
+
+// function closeFull() {
+//   full.classList.toggle('full_opened');
+// }
+
+// fullCloseBtn.addEventListener('click', closeFull);
