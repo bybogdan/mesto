312f11e9481d60600b3053c8e81621b72e14c7d8
bogdan <varlamovbogdan@yandex.ru>
2020-06-25 12:05:52 +0400

    project ready for

--- scripts/openFullPicture.js
+++
@@ -1,8 +0,0 @@
-// const full = document.querySelector('.full');
-// const fullCloseBtn = full.querySelector('.full__close');
-
-// function closeFull() {
-//   full.classList.toggle('full_opened');
-// }
-
-// fullCloseBtn.addEventListener('click', closeFull);
