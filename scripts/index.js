f089981dbbc7fb661907f9c97bafd62a3c7caa7d
bogdan <varlamovbogdan@yandex.ru>
2020-06-25 17:06:13 +0400

    fix: deleted utils.js file, in index.html have one connected file index.js/ array with data in index.js/ all quotes are single

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,41 @@
-const buttonEdit = document.querySelector(".profile__edit-button");
-const popupForm = document.querySelector(".popup__form");
-const nameProfile = document.querySelector(".profile__title");
-const captionProfile = document.querySelector(".profile__subtitle");
-const popup = document.querySelector(".popup");
-const inputName = popup.querySelector(".popup__input_text_name");
-const inputCaption = popup.querySelector(".popup__input_text_caption");
+// массив
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-const buttonSave = popup.querySelector(".popup__button-save");
-const buttonExit = popup.querySelector(".popup__button-exit");
+const buttonEdit = document.querySelector('.profile__edit-button');
+const popupForm = document.querySelector('.popup__form');
+const nameProfile = document.querySelector('.profile__title');
+const captionProfile = document.querySelector('.profile__subtitle');
+const popup = document.querySelector('.popup');
+const inputName = popup.querySelector('.popup__input_text_name');
+const inputCaption = popup.querySelector('.popup__input_text_caption');
+
+const buttonSave = popup.querySelector('.popup__button-save');
+const buttonExit = popup.querySelector('.popup__button-exit');
 
 // доступ к template content
 const card = document.querySelector('#card').content;
@@ -31,7 +59,7 @@ const cardTitle = document.querySelector('.popup-add__input_text_name');
 const cardSrc = document.querySelector('.popup-add__input_text_caption');
 
 function popupToggle() {
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle('popup_opened');
   inputName.value = nameProfile.textContent;
   inputCaption.value = captionProfile.textContent;
 }
@@ -185,7 +213,7 @@ popupAddForm.addEventListener('submit', function (event) {
 // вызов функция создания страртовых шести карточек
 createStartersCard();
 
-buttonEdit.addEventListener("click", popupToggle);
-buttonExit.addEventListener("click", popupToggle);
-popupForm.addEventListener("submit", saveChangesPopup);
-popup.addEventListener("click", CloseAround);
+buttonEdit.addEventListener('click', popupToggle);
+buttonExit.addEventListener('click', popupToggle);
+popupForm.addEventListener('submit', saveChangesPopup);
+popup.addEventListener('click', CloseAround);
